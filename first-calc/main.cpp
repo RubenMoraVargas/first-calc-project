8955603ced4a5f55696500dae05a5800c82e8684
Rubén Mora Vargas <rubenmoravargas@gmail.com>
2024-03-16 13:17:13 -0600

    Add and test the function substract

--- first-calc/main.cpp
+++ first-calc/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
  
+
 static int add(int first, int second) {
 	return first + second;
 }
-
+static int substract(int first, int second) {
+    return first - second;
+}
 static void testAdd() {
     int result1 = add(4, 6);
     std::cout << "Prueba #1, la suma es: " << result1 << std::endl;
@@ -13,7 +16,14 @@ static void testAdd() {
     int result3 = add(0, 98);
     std::cout << "Prueba #3, la suma es: " << result3 << std::endl;
 }
-
+static void testSubstract() {
+    int result1 = substract(4, 6);
+    std::cout << "Prueba #1, la resta es: " << result1 << std::endl;
+    int result2 = substract(15, -15);
+    std::cout << "Prueba #2, la resta es: " << result2 << std::endl;
+    int result3 = substract(0, 98);
+    std::cout << "Prueba #3, la resta es: " << result3 << std::endl;
+}
 int main() {
-    testAdd();
+    testSubstract();
 }
