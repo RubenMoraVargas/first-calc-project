710c13d06285e5480916f62b42b9c621c89efa58
Rubén Mora Vargas <rubenmoravargas@gmail.com>
2024-03-16 13:15:53 -0600

    Add and test function add

--- first-calc/main.cpp
+++ first-calc/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
-  
+ 
+static int add(int first, int second) {
+	return first + second;
+}
+
+static void testAdd() {
+    int result1 = add(4, 6);
+    std::cout << "Prueba #1, la suma es: " << result1 << std::endl;
+    int result2 = add(15, -15);
+    std::cout << "Prueba #2, la suma es: " << result2 << std::endl;
+    int result3 = add(0, 98);
+    std::cout << "Prueba #3, la suma es: " << result3 << std::endl;
+}
+
 int main() {
-	cout << "Bien hecho" << endl;
-	return 0;
+    testAdd();
 }
