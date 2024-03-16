0787a6eb4ef5eb120b5562cf11e5bd41da69b342
Rubén Mora Vargas <rubenmoravargas@gmail.com>
2024-03-16 13:19:00 -0600

    Add and test the function printMenu

--- first-calc/main.cpp
+++ first-calc/main.cpp
@@ -8,6 +8,19 @@ static int add(int first, int second) {
 static int substract(int first, int second) {
     return first - second;
 }
+
+static void printMenu() {
+    std::cout << "Calculadora en consola        \n";
+    std::cout << "==============================\n";
+    std::cout << "Dadas las siguientes opciones:\n";
+    std::cout << "1. Sumar\n";
+    std::cout << "2. Restar\n";
+    std::cout << "3. Multiplicar\n";
+    std::cout << "4. Dividir\n";
+    std::cout << "5. Potencia\n";
+    std::cout << "6. Salir\n";
+}
+
 static void testAdd() {
     int result1 = add(4, 6);
     std::cout << "Prueba #1, la suma es: " << result1 << std::endl;
@@ -16,6 +29,7 @@ static void testAdd() {
     int result3 = add(0, 98);
     std::cout << "Prueba #3, la suma es: " << result3 << std::endl;
 }
+
 static void testSubstract() {
     int result1 = substract(4, 6);
     std::cout << "Prueba #1, la resta es: " << result1 << std::endl;
@@ -24,6 +38,8 @@ static void testSubstract() {
     int result3 = substract(0, 98);
     std::cout << "Prueba #3, la resta es: " << result3 << std::endl;
 }
+
 int main() {
-    testSubstract();
+    printMenu();  
+    return 0;
 }
